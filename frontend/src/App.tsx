238773bb6b61b6a955ce41ea29881ffc658c9275
Fishon Amos <43862685+fishonamos@users.noreply.github.com>
2024-10-07 10:10:38 +0100

    Revert "Add Contributor bot "

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/NavBar";
+import Hero from "./components/Hero";
+import WhyChooseLandVer from "./components/WhyChooseLandver";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <WhyChooseLandVer/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
